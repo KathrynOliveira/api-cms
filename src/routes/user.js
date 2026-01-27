e9cf982188deea7f50902ff772ed6f7152d735df
KathrynOliveira <k.olliver18@gmail.com>
2026-01-27 11:55:31 -0300

    refactor: remove unused user routes and server file

--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userValidation = require("../middlewares/validations/userValidation");
-const UserController = require("../controllers/UserController");
-
-router.post("/users", userValidation, UserController.createUser);
-
-module.exports = router;
