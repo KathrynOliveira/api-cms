a76bf3c75efe8bf24669b27f7d953cd7d1704aca
KathrynOliveira <k.olliver18@gmail.com>
2026-02-02 11:18:18 -0300

    feat: add Swagger UI integration and API documentation in swagger.yaml

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import helmet from "helmet";
+
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import categoriesRoutes from "./routes/categoriesRoutes";
 import articleRoutes from "./routes/articleRoutes";
+
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(helmet());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(authRoutes);
 app.use(userRoutes);
 app.use(categoriesRoutes);
